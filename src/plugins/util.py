7b7a07d6917ad1bde140ac049715d33ac95acc29
One-Nub <38899321+One-Nub@users.noreply.github.com>
2019-06-26 12:21:18 -0500

    Create a basic ping command

--- src/plugins/util.py
+++ src/plugins/util.py
@@ -52,3 +52,8 @@ class Utilities(Plugin):
 
         infoEmbed.add_field(name = "View my github here!", value = github, inline = False)
         event.msg.reply(embed = infoEmbed)
+
+    @Plugin.command("ping", aliases = ["pong"], group = "test")
+    def on_ping_command(self, event):
+        """Responds with 'Pong!' if the bot is online"""
+        event.msg.reply("Pong!")
