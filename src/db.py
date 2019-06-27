223ef6a628a9d42bfaed8204771f1268f41b66b4
One-Nub <38899321+One-Nub@users.noreply.github.com>
2019-06-27 17:31:25 -0500

    Create mariadb_funcs class
    Also create the methods inside the class including the completion of
    get_user method

--- src/db.py
+++ src/db.py
@@ -1,7 +1,8 @@
 import json
 import mysql.connector as mariadb
+from disco.bot import Plugin
 
-class DB_CREDS():
+class DB_CREDS:
     name = ""
     password = ""
     address = ""
@@ -43,3 +44,38 @@ class DB_CREDS():
 
     def set_db(self, db):
         self.database = db
+
+class mariadb_funcs(Plugin):
+    dbclass = DB_CREDS()
+    connection = mariadb.connect(user = dbclass.get_name(), password = dbclass.get_pass(),
+    host = dbclass.get_host(), database = dbclass.get_db())
+
+    def update_server_table(self, serverID, prefix):
+        pass
+
+    def get_user(self, userID):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM user_settings WHERE user_id=%s", (userID,))
+        row = cursor.fetchone()
+        if row is None:
+            try:
+                cursor.execute("INSERT INTO user_settings (user_id) VALUES (%s)", (userID,))
+                self.connection.commit()
+                print("new user added: {}".format(userID))
+            except mariadb.Error as error:
+                print("New user insert error: {}".format(error))
+
+            cursor.execute("SELECT * FROM user_settings WHERE user_id=%s", (userID,))
+            row = cursor.fetchone()
+
+        cursor.close()
+        return row
+
+    def update_user_setting(self, userID, option):
+        row = self.get_user(userID)
+        print(row)
+        #cursor = self.connection.cursor()
+        #Option has to be: call_me, usr_desc, timezone, facts (associates to columns in database)
+        #try:
+        #    cursor.execute
+        #pass
